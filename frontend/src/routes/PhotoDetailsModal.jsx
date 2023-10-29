3d6e3a64a277c167dc885d72051ff2ddcc3870c1
eusoncode <git@github.com:eusoncode/Kata.git>
2023-10-29 15:06:31 -0600

    Edited 'ListOfPhotos props and removed isModalOpen

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,30 +5,28 @@ import "../styles/PhotoListItem.scss";
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from 'components/PhotoList';
 
-const PhotoDetailsModal = ({ clickedPhoto, isModalOpen, closeModal, toggleFavImage, isActive }) => { 
+const PhotoDetailsModal = ({ clickedPhoto, closeModal, toggleFavImage, isActive }) => { 
   return (
-    // Render the modal only if isModalOpen is true
-    isModalOpen && (
-      <div className="photo-details-modal">
-        <button className="photo-details-modal__close-button" onClick={closeModal}>
-          <img src={closeSymbol} alt="close symbol" />
-        </button>
-        <div className='photo-details-modal__images'>
-          <img className="photo-details-modal__image" src={clickedPhoto.urls.full} />
-          <section className="photo-details-modal__photographer-details">
-            <img className="photo-details-modal__photographer-profile" src={clickedPhoto.user.profile} />
-            <div className="photo-details-modal__photographer-info">
-              <p className="photo-details-modal__photographer-name">{clickedPhoto.user.name}</p>
-              <p className="photo-details-modal__photographer-location" >{`${clickedPhoto.location.city}, ${clickedPhoto.location.country}`}</p>
-            </div>        
-          </section>
-          <p className='photo-details-modal__header'>Similar Photos</p>
-          <section className='photo-details-modal__images' >
-            <PhotoList listOfPhotos={[...Object.values(clickedPhoto.similar_photos)]} toggleFavImage={toggleFavImage} isActive={isActive} />
-          </section>     
-        </div>        
-      </div>
-    )
+    // Render the modal
+    <div className="photo-details-modal">
+      <button className="photo-details-modal__close-button" onClick={closeModal}>
+        <img src={closeSymbol} alt="close symbol" />
+      </button>
+      <div className='photo-details-modal__images'>
+        <img className="photo-details-modal__image" src={clickedPhoto.urls.full} />
+        <section className="photo-details-modal__photographer-details">
+          <img className="photo-details-modal__photographer-profile" src={clickedPhoto.user.profile} />
+          <div className="photo-details-modal__photographer-info">
+            <p className="photo-details-modal__photographer-name">{clickedPhoto.user.name}</p>
+            <p className="photo-details-modal__photographer-location" >{`${clickedPhoto.location.city}, ${clickedPhoto.location.country}`}</p>
+          </div>        
+        </section>
+        <p className='photo-details-modal__header'>Similar Photos</p>
+        <section className='photo-details-modal__images' >
+          <PhotoList listOfPhotos={clickedPhoto.similar_photos} toggleFavImage={toggleFavImage} isActive={isActive} />
+        </section>     
+      </div>        
+    </div>
   );
 };
 
