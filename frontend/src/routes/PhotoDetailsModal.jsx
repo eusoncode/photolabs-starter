e9c55ab1c013e0e2eea83c912f55a441894c23a1
eusoncode <git@github.com:eusoncode/Kata.git>
2023-10-29 15:05:15 -0600

    Applied the right scss to large image and added 'similar photos'

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -13,17 +13,20 @@ const PhotoDetailsModal = ({ clickedPhoto, isModalOpen, closeModal, toggleFavIma
         <button className="photo-details-modal__close-button" onClick={closeModal}>
           <img src={closeSymbol} alt="close symbol" />
         </button>
-        <img className="photo-details-modal__image" src={clickedPhoto.urls.full} />
-        <section className="photo-details-modal__photographer-details">
-          <img className="photo-details-modal__photographer-profile" src={clickedPhoto.user.profile} />
-          <div className="photo-details-modal__photographer-info">
-            <p className="photo-details-modal__photographer-name">{clickedPhoto.user.name}</p>
-            <p className="photo-details-modal__photographer-location" >{`${clickedPhoto.location.city}, ${clickedPhoto.location.country}`}</p>
-          </div>        
-        </section>
-        <section className='photo-details-modal__images' >
-          <PhotoList listOfPhotos={[...Object.values(clickedPhoto.similar_photos)]} toggleFavImage={toggleFavImage} isActive={isActive} />
-        </section>     
+        <div className='photo-details-modal__images'>
+          <img className="photo-details-modal__image" src={clickedPhoto.urls.full} />
+          <section className="photo-details-modal__photographer-details">
+            <img className="photo-details-modal__photographer-profile" src={clickedPhoto.user.profile} />
+            <div className="photo-details-modal__photographer-info">
+              <p className="photo-details-modal__photographer-name">{clickedPhoto.user.name}</p>
+              <p className="photo-details-modal__photographer-location" >{`${clickedPhoto.location.city}, ${clickedPhoto.location.country}`}</p>
+            </div>        
+          </section>
+          <p className='photo-details-modal__header'>Similar Photos</p>
+          <section className='photo-details-modal__images' >
+            <PhotoList listOfPhotos={[...Object.values(clickedPhoto.similar_photos)]} toggleFavImage={toggleFavImage} isActive={isActive} />
+          </section>     
+        </div>        
       </div>
     )
   );
