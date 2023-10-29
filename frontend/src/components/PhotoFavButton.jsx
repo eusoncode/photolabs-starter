69c1500679e003aaf42873e94272b51e9415428b
eusoncode <git@github.com:eusoncode/Kata.git>
2023-10-29 15:48:54 -0600

    Added FavIcon to PhotoFavButton

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import '../styles/PhotoFavButton.scss';
+import FavIcon from './FavIcon';
 
 // Component for a favorite button with the ability to change its color
 function PhotoFavButton({ isActive, handleIconClick }) {
-  
-  // Define the source for the heart icon based on isActive
-  const heartIconSrc = isActive
-    ? 'https://www.svgrepo.com/show/303487/heart-logo.svg'
-    : 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Heart_icon_red_hollow.svg/812px-Heart_icon_red_hollow.svg.png';
 
   return (
     <div className="photo-list__fav-icon" onClick={handleIconClick}>
       <div className="photo-list__fav-icon-svg">
-        <img src={heartIconSrc} alt="Favorite Icon" style={{ width: '20px', height: '20px' }} />
+        <FavIcon selected={isActive}/>
       </div>
     </div>
   );
